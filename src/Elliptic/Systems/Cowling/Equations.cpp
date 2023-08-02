763c300a451de91683ea67b828b48ae5c55e6b27
PeterJames Nee <pjnee@live.ie>
2023-08-02 11:44:11 +0100

    First step towards Cowling approximation.

---
+++ src/Elliptic/Systems/Cowling/Equations.cpp
@@ -0,0 +1,140 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Elliptic/Systems/Poisson/Equations.hpp"
+
+#include <cstddef>
+
+#include "DataStructures/DataBox/PrefixHelpers.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "DataStructures/Tensor/EagerMath/DotProduct.hpp"
+#include "DataStructures/Tensor/Tensor.hpp"
+#include "PointwiseFunctions/GeneralRelativity/IndexManipulation.hpp"
+#include "Utilities/GenerateInstantiations.hpp"
+#include "Utilities/MakeWithValue.hpp"
+
+namespace Poisson {
+
+template <size_t Dim>
+void flat_cartesian_fluxes(
+    const gsl::not_null<tnsr::I<DataVector, Dim>*> flux_for_field,
+    const tnsr::i<DataVector, Dim>& field_gradient) {
+  for (size_t d = 0; d < Dim; d++) {
+    flux_for_field->get(d) = field_gradient.get(d);
+  }
+}
+
+template <size_t Dim>
+void curved_fluxes(
+    const gsl::not_null<tnsr::I<DataVector, Dim>*> flux_for_field,
+    const tnsr::II<DataVector, Dim>& inv_spatial_metric,
+    const tnsr::i<DataVector, Dim>& field_gradient) {
+  raise_or_lower_index(flux_for_field, field_gradient, inv_spatial_metric);
+}
+
+template <size_t Dim>
+void add_curved_sources(
+    const gsl::not_null<Scalar<DataVector>*> source_for_field,
+    const tnsr::i<DataVector, Dim>& christoffel_contracted,
+    const tnsr::I<DataVector, Dim>& flux_for_field) {
+  get(*source_for_field) -=
+      get(dot_product(christoffel_contracted, flux_for_field));
+}
+
+template <size_t Dim>
+void auxiliary_fluxes(
+    gsl::not_null<tnsr::Ij<DataVector, Dim>*> flux_for_gradient,
+    const Scalar<DataVector>& field) {
+  std::fill(flux_for_gradient->begin(), flux_for_gradient->end(), 0.);
+  for (size_t d = 0; d < Dim; d++) {
+    flux_for_gradient->get(d, d) = get(field);
+  }
+}
+
+template <size_t Dim>
+void Fluxes<Dim, Geometry::FlatCartesian>::apply(
+    const gsl::not_null<tnsr::I<DataVector, Dim>*> flux_for_field,
+    const tnsr::i<DataVector, Dim>& field_gradient) {
+  flat_cartesian_fluxes(flux_for_field, field_gradient);
+}
+
+template <size_t Dim>
+void Fluxes<Dim, Geometry::Curved>::apply(
+    const gsl::not_null<tnsr::I<DataVector, Dim>*> flux_for_field,
+    const tnsr::II<DataVector, Dim>& inv_spatial_metric,
+    const tnsr::i<DataVector, Dim>& field_gradient) {
+  curved_fluxes(flux_for_field, inv_spatial_metric, field_gradient);
+}
+
+template <size_t Dim>
+void Fluxes<Dim, Geometry::FlatCartesian>::apply(
+    const gsl::not_null<tnsr::Ij<DataVector, Dim>*> flux_for_gradient,
+    const Scalar<DataVector>& field) {
+  auxiliary_fluxes(flux_for_gradient, field);
+}
+
+template <size_t Dim>
+void Fluxes<Dim, Geometry::Curved>::apply(
+    const gsl::not_null<tnsr::Ij<DataVector, Dim>*> flux_for_gradient,
+    const tnsr::II<DataVector, Dim>& /*inv_spatial_metric*/,
+    const Scalar<DataVector>& field) {
+  auxiliary_fluxes(flux_for_gradient, field);
+}
+
+template <size_t Dim>
+void Sources<Dim, Geometry::FlatCartesian>::apply(
+    const gsl::not_null<Scalar<DataVector>*> /*equation_for_field*/,
+    const Scalar<DataVector>& /*field*/,
+    const tnsr::I<DataVector, Dim>& /*field_flux*/) {}
+
+template <size_t Dim>
+void Sources<Dim, Geometry::Curved>::apply(
+    const gsl::not_null<Scalar<DataVector>*> equation_for_field,
+    const tnsr::i<DataVector, Dim>& christoffel_contracted,
+    const Scalar<DataVector>& /*field*/,
+    const tnsr::I<DataVector, Dim>& field_flux) {
+  add_curved_sources(equation_for_field, christoffel_contracted, field_flux);
+}
+
+template <size_t Dim>
+void Sources<Dim, Geometry::FlatCartesian>::apply(
+    const gsl::not_null<
+        tnsr::i<DataVector, Dim>*> /*equation_for_field_gradient*/,
+    const Scalar<DataVector>& /*field*/) {}
+
+template <size_t Dim>
+void Sources<Dim, Geometry::Curved>::apply(
+    const gsl::not_null<
+        tnsr::i<DataVector, Dim>*> /*equation_for_field_gradient*/,
+    const tnsr::i<DataVector, Dim>& /*christoffel_contracted*/,
+    const Scalar<DataVector>& /*field*/) {}
+
+}  // namespace Poisson
+
+#define DIM(data) BOOST_PP_TUPLE_ELEM(0, data)
+
+#define INSTANTIATE(_, data)                                                   \
+  template void Poisson::flat_cartesian_fluxes<DIM(data)>(                     \
+      const gsl::not_null<tnsr::I<DataVector, DIM(data)>*>,                    \
+      const tnsr::i<DataVector, DIM(data)>&);                                  \
+  template void Poisson::curved_fluxes<DIM(data)>(                             \
+      const gsl::not_null<tnsr::I<DataVector, DIM(data)>*>,                    \
+      const tnsr::II<DataVector, DIM(data)>&,                                  \
+      const tnsr::i<DataVector, DIM(data)>&);                                  \
+  template void Poisson::add_curved_sources<DIM(data)>(                        \
+      const gsl::not_null<Scalar<DataVector>*>,                                \
+      const tnsr::i<DataVector, DIM(data)>&,                                   \
+      const tnsr::I<DataVector, DIM(data)>&);                                  \
+  template void Poisson::auxiliary_fluxes<DIM(data)>(                          \
+      gsl::not_null<tnsr::Ij<DataVector, DIM(data)>*>,                         \
+      const Scalar<DataVector>&);                                              \
+  template class Poisson::Fluxes<DIM(data), Poisson::Geometry::FlatCartesian>; \
+  template class Poisson::Fluxes<DIM(data), Poisson::Geometry::Curved>;        \
+  template class Poisson::Sources<DIM(data),                                   \
+                                  Poisson::Geometry::FlatCartesian>;           \
+  template class Poisson::Sources<DIM(data), Poisson::Geometry::Curved>;
+
+GENERATE_INSTANTIATIONS(INSTANTIATE, (1, 2, 3))
+
+#undef INSTANTIATE
+#undef DIM
---
+++ src/Elliptic/Systems/Cowling/Tags.hpp
@@ -0,0 +1,88 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+/// \file
+/// Defines DataBox tags for the Cowling system
+
+#pragma once
+
+#include <string>
+
+#include "DataStructures/DataBox/Tag.hpp"
+#include "DataStructures/Tensor/TypeAliases.hpp"
+#include "Options/Options.hpp"
+
+/// \cond
+class DataVector;
+/// \endcond
+
+/*!
+ * \ingroup EllipticSystemsGroup
+ * \brief Items related to solving a Cowling equation \f$-\Delta u(x)=f(x)\f$.
+ */
+namespace Cowling {
+
+namespace OptionTags {
+
+/*!
+ * \brief The maximum number of self-consistent iterations to be done.
+ */
+struct MaxIterations {
+  static std::string name() { return "MaxIterations"; }
+  using type = size_t;
+  static constexpr Options::String help{
+      "The maximum number of self-consistent iterations to be done."};
+};
+
+/*!
+ * \brief Epsilon for self-consistent iterations.
+ */
+struct Epsilon {
+  static std::string name() { return "Epsilon"; }
+  using type = double;
+  static constexpr Options::String help{
+      "Epsilon for self-consistent iterations."};
+};
+
+}  // namespace OptionTags
+namespace Tags {
+
+/*!
+ * \brief The scalar field \f$u(x)\f$ to solve for
+ */
+struct Field : db::SimpleTag {
+  using type = Scalar<DataVector>;
+};
+
+/*!
+ * \brief The number of self consistent iterations done.
+ */
+struct SolveIteration : db::SimpleTag {
+  using type = size_t;
+};
+
+/*!
+ * \brief The maximum number of self-consistent iterations to be done.
+ */
+struct MaxIterations : db::SimpleTag {
+  using type = size_t;
+  using option_tags = tmpl::list<OptionTags::MaxIterations>;
+  static constexpr bool pass_metavariables = false;
+  static size_t create_from_options(const size_t max_iterations) {
+    return max_iterations;
+  }
+};
+
+/*!
+ * \brief Epsilon for self-consistent iterations.
+ */
+struct Epsilon : db::SimpleTag {
+  using type = double;
+  using option_tags = tmpl::list<OptionTags::Epsilon>;
+  static constexpr bool pass_metavariables = false;
+  static double create_from_options(const double epsilon) { return epsilon; }
+};
+
+}  // namespace Tags
+
+}  // namespace Cowling
