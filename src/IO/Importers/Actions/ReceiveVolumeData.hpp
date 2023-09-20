73bb32237d26b4a6f78c7f372052cb09ad08820b
PeterJames Nee <pjnee@live.ie>
2023-09-20 09:24:52 +0200

    Compiles!

--- src/IO/Importers/Actions/ReceiveVolumeData.hpp
+++ src/IO/Importers/Actions/ReceiveVolumeData.hpp
@@ -51,13 +51,11 @@ struct ReceiveVolumeData {
     auto& element_data = received_data->second;
     tmpl::for_each<FieldTagsList>([&box, &element_data](auto tag_v) {
       using tag = tmpl::type_from<decltype(tag_v)>;
-      // db::mutate<tag>(
-      //     [&element_data](const gsl::not_null<typename tag::type*> value) {
-      //       *value = std::move(tuples::get<tag>(element_data));
-      //     },
-      //     make_not_null(&box));
-      ::Initialization::mutate_assign<tmpl::list<tag>>(
-          make_not_null(&box), tuples::get<tag>(element_data));
+      db::mutate<tag>(
+          [&element_data](const gsl::not_null<typename tag::type*> value) {
+            *value = std::move(tuples::get<tag>(element_data));
+          },
+          make_not_null(&box));
     });
     inbox.erase(received_data);
     return {Parallel::AlgorithmExecution::Continue, std::nullopt};
