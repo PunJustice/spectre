352580272d2f20d48ee6754c55e3799174f9123c
PeterJames Nee <pjnee@live.ie>
2023-10-19 14:47:18 +0200

    oops.

--- src/PointwiseFunctions/AnalyticSolutions/Cowling/Factory.hpp
+++ src/PointwiseFunctions/AnalyticSolutions/Cowling/Factory.hpp
@@ -8,7 +8,7 @@
 #include "PointwiseFunctions/AnalyticData/Xcts/Binary.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/Cowling/Gaussian.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/Cowling/Inverser.hpp"
-// #include "PointwiseFunctions/AnalyticSolutions/Cowling/KerrSchildCowling.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/Cowling/SuperposedInverser.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/Cowling/Zero.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/HarmonicSchwarzschild.hpp"
 #include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/KerrSchild.hpp"
@@ -22,5 +22,6 @@ namespace Cowling::Solutions {
 using all_analytic_solutions =
     tmpl::list<Xcts::Solutions::WrappedGr<gr::Solutions::KerrSchild>>;
 template <size_t Dim>
-using all_initial_guesses = tmpl::list<Zero<Dim>, Inverser<Dim>, Gaussian>;
+using all_initial_guesses =
+    tmpl::list<Zero<Dim>, Inverser<Dim>, Gaussian, SuperposedInverser<Dim>>;
 }  // namespace Cowling::Solutions
