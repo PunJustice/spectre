d704d8ffafcb47233ace648844a8cf33a32577de
PeterJames Nee <pjnee@live.ie>
2024-03-11 14:05:15 +0100

    Damping mistake.

--- src/Elliptic/Systems/Cowling/Actions/IterativeSolve.hpp
+++ src/Elliptic/Systems/Cowling/Actions/IterativeSolve.hpp
@@ -120,15 +120,17 @@ struct IterativeSolve {
     }
 
     db::mutate<fixed_sources_tag, ::Cowling::Tags::SolveIteration,
-               Cowling::Tags::PreviousSolve>(
+               Cowling::Tags::PreviousSolve, ::CurvedScalarWave::Tags::Psi>(
         [](const gsl::not_null<Scalar<DataVector>*> field,
            const gsl::not_null<size_t*> solve_iteration,
            const gsl::not_null<Scalar<DataVector>*> previous_solve_field,
+           const gsl::not_null<Scalar<DataVector>*> solution,
            const Scalar<DataVector> field_value, const double iteration_value,
            const Scalar<DataVector> solve_value) {
           *field = field_value;
           *solve_iteration = iteration_value;
           *previous_solve_field = solve_value;
+          *solution = solve_value;
         },
         make_not_null(&box), new_source, iteration,
         Scalar<DataVector>{update_field});
