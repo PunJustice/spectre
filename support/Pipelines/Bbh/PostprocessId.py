fd31958d40480069d9c7a0c489aa712a0ad17705
PeterJames Nee <pjnee@live.ie>
2024-11-21 19:07:18 +0100

    One more bug.

--- support/Pipelines/Bbh/PostprocessId.py
+++ support/Pipelines/Bbh/PostprocessId.py
@@ -22,7 +22,7 @@ from spectre.Pipelines.Bbh.FindHorizon import (
 )
 from spectre.Pipelines.Bbh.SolveST import prepare_scalar_solve
 from spectre.PointwiseFunctions.ScalarTensor import st_charge
-from spectre.SphericalHarmonics import Strahlkorper
+from spectre.SphericalHarmonics import Strahlkorper, cartesian_coords
 from spectre.support.Schedule import schedule, scheduler_options
 from spectre.Visualization.OpenVolfiles import open_volfiles
 from spectre.Visualization.ReadH5 import select_observation
