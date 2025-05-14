6fc45c0c6b1d75703f76e766a67a8e634e0356a9
PeterJames Nee <peter.nee@aei.mpg.de>
2025-05-14 16:39:19 +0200

    Working urania compilation.

--- support/Environments/urania.sh
+++ support/Environments/urania.sh
@@ -5,25 +5,30 @@
 
 spectre_setup_charm_paths() {
     # Define Charm paths
-    export CHARM_ROOT=/u/guilara/charm_impi_2/mpi-linux-x86_64-smp
-    export PATH=$PATH:/u/guilara/charm_impi_2/mpi-linux-x86_64-smp/bin
+    export CHARM_ROOT=/u/guilara/charm_impi_3/mpi-linux-x86_64-smp
+    export PATH=$PATH:/u/guilara/charm_impi_3/mpi-linux-x86_64-smp/bin
 }
 
 spectre_load_modules() {
+    module purge
     module load gcc/11
     module load impi/2021.7
+    # module load openmpi/4
     module load boost/1.79
+    # module load doxygen/1.9.5
     module load gsl/1.16
     module load cmake/3.26
     module load hdf5-serial/1.12.2
     module load anaconda/3/2021.11
+    # For plots and visualization
     module load paraview/5.10
-    # Load Spack environment
+    module load texlive/2021
+    module load gnuplot/5.4
+    #
     source /u/guilara/repos/spack/share/spack/setup-env.sh
-    source /urania/u/guilara/repos/spack/var/spack/environments\
-/env3_spectre_impi/loads
-    # Load python environment
-    source /u/guilara/envs/spectre_env/bin/activate
+    spack env activate env3_spectre_impi
+    export SPECTRE_HOME=/u/pjn/gitrepos/spectre/combined_fork
+    source /u/guilara/repos/spectre/env/bin/activate
 }
 
 spectre_unload_modules() {
@@ -46,24 +51,22 @@ spectre_run_cmake() {
         echo "You must set SPECTRE_HOME to the cloned SpECTRE directory"
         return 1
     fi
+    spectre_setup_charm_paths
     spectre_load_modules
 
-    cmake -D CMAKE_C_COMPILER=gcc \
-          -D CMAKE_CXX_COMPILER=g++ \
-          -D CMAKE_Fortran_COMPILER=gfortran \
-          -D CHARM_ROOT=$CHARM_ROOT \
-          -D CMAKE_BUILD_TYPE=Release \
-          -D DEBUG_SYMBOLS=OFF \
-          -D BUILD_SHARED_LIBS=ON \
-          -D MEMORY_ALLOCATOR=JEMALLOC \
-          -D BUILD_PYTHON_BINDINGS=ON \
-          -D MACHINE=Urania \
-          -D Catch2_DIR=/u/guilara/repos/Catch2/install_dir/lib64/cmake/Catch2 \
-          -D MPI_C_COMPILER=/mpcdf/soft/SLE_15/packages/skylake\
-/impi/gcc_11-11.2.0/2021.7.1/bin/mpigcc \
-          -D MPI_CXX_COMPILER=/mpcdf/soft/SLE_15/packages/skylake\
-/impi/gcc_11-11.2.0/2021.7.1/bin/mpig++ \
-          -D MPI_Fortran_COMPILER=/mpcdf/soft/SLE_15/packages/skylake\
-/impi/gcc_11-11.2.0/2021.7.1/bin/mpigfortran \
-          -Wno-dev "$@" $SPECTRE_HOME
+    cmake -D CMAKE_C_COMPILER=gcc -D CMAKE_CXX_COMPILER=g++ \
+    -D CMAKE_Fortran_COMPILER=gfortran \
+    -D CHARM_ROOT=$CHARM_ROOT -D CMAKE_BUILD_TYPE=Release -D DEBUG_SYMBOLS=OFF \
+    -D BUILD_SHARED_LIBS=ON \
+    -D MEMORY_ALLOCATOR=JEMALLOC \
+    -D BUILD_PYTHON_BINDINGS=ON \
+    -D Catch2_DIR=/u/guilara/repos/Catch2/install_dir/lib64/cmake/Catch2 \
+    -D MPI_C_COMPILER=/mpcdf/soft/SLE_15/packages/skylake/impi/\
+    gcc_11-11.2.0/2021.7.1/bin/mpigcc \
+    -D MPI_CXX_COMPILER=/mpcdf/soft/SLE_15/packages/skylake/impi/\
+    gcc_11-11.2.0/2021.7.1/bin/mpig++ \
+    -D MPI_Fortran_COMPILER=/mpcdf/soft/SLE_15/packages/skylake/impi/\
+    gcc_11-11.2.0/2021.7.1/bin/mpigfortran \
+    -D SPEC_ROOT=/u/guilara/repos/spec -D MACHINE=Urania \
+    -Wno-dev "$@" $SPECTRE_HOME
 }
